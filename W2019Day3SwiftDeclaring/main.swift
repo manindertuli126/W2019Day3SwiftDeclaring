bee5cdf8561737824322160bd7cfcf2bc4f31164
MacStudent <macstudent@126-iMac05.local>
2019-02-11 18:39:02 -0500

    Initial Commit

---
+++ W2019Day3SwiftDeclaring/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  W2019Day3SwiftDeclaring
+//
+//  Created by MacStudent on 2019-02-11.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
