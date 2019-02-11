374dae1a744d8405334427d9d8767831fcfc0bed
MacStudent <macstudent@126-iMac05.local>
2019-02-11 18:40:47 -0500

    Renaming the creator of project

--- W2019Day3SwiftDeclaring/main.swift
+++ W2019Day3SwiftDeclaring/main.swift
@@ -2,11 +2,11 @@
 //  main.swift
 //  W2019Day3SwiftDeclaring
 //
-//  Created by MacStudent on 2019-02-11.
+//  Created by Maninder on 2019-02-11.
 //  Copyright © 2019 MacStudent. All rights reserved.
 //
 
 import Foundation
 
-print("Hello, World!")
+print("Maninder Singh")
 
