4d69c1eaf029f2f8ec7fb25f8199d616c7cdb241
MacStudent <macstudent@126-iMac05.local>
2019-02-11 18:56:39 -0500

    Added code for declaring functions with examples.

--- W2019Day3SwiftDeclaring/main.swift
+++ W2019Day3SwiftDeclaring/main.swift
@@ -7,6 +7,37 @@
 //
 
 import Foundation
+// declaring a function
 
-print("Maninder Singh")
+func hello(){
+    print("**** First Function creation and calling ****")
+}
 
+hello()
+
+func sum(a: Int, b: Int){
+    let c = a+b
+    print("Sum Int: \(c)")
+}
+sum(a: 10,b: 20)
+//sum(b: 10,a: 20) Incorrect format
+
+//renaming the variable labels
+func sum(ofA a: Float, andB b: Float){
+    let c = a+b
+    print("Sum Float: \(c)")
+}
+sum(ofA: 10.3,andB: 20.4)
+
+func sub(a: Int, b: Int){
+    let c = a-b
+    print("Sub Int: \(c)")
+}
+sub(a: 20,b: 10)
+
+// without label use underscore
+func sub(_ a: Int, _ b: Int){
+    let c = a-b
+    print("Sub Int withot label: \(c)")
+}
+sub(20, 10)
